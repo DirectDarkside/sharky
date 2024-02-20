5a382ff95f35e70b8f927a0890cefdfaf748883f
DirectDarkside <109902468+DirectDarkside@users.noreply.github.com>
2024-02-20 22:35:14 +0100

    add img in character and chickens

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-
+    world = new World();
     
-    character.src = '../assets/img/1.Sharkie/1.IDLE/1.png'
 
-    ctx.drawImage(character, 20, 20, 150, 150);
+    console.log('My Character is', world.character);
 }
