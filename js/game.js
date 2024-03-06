b5ed81ec67a29e5a816be74c931f35c99c5d038a
DirectDarkside <florianmkube13@gmail.com>
2024-03-06 10:21:15 +0100

    add fullscreen

--- js/game.js
+++ js/game.js
@@ -65,3 +65,29 @@ document.addEventListener('keyup', (event) => {
     }
     
 });
+
+function fullscreen() {
+    const fullscreen = document.getElementById('canvas_container');
+    openFullscreen(fullscreen);
+}
+
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+  
+  /* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
